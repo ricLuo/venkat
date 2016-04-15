6a646704ac3300bc09c374e47416b23bc8c74706
ricace <vigric@outlook.com>
2016-04-15 15:22:06 -0400

    Add files about pages, routes and mongoose model

--- app/models/User.js
+++ app/models/User.js
@@ -1,12 +1,15 @@
 // grab the mongoose module
 var mongoose = require('mongoose');
-
+var crypto = require('crypto');
 // define User model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('User', {
+
+var UserSchema = new mongoose.Schema({
   username: String,
   password: String,
   type: String,
+  email:String,
+  head:String,
   tests: [{
     title: String,
     questions : [{
@@ -19,3 +22,59 @@ module.exports = mongoose.model('User', {
     }]
   }]
 });
+
+UserSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+var userModel = mongoose.model('User', UserSchema);
+
+function User(user){
+    this.username = user.Title;
+    this.password = user.password;
+    this.email = user.email;
+    this.head = user.head;
+    this.tests = user.tests;
+}
+
+User.prototype.save = function(callback){
+    var md5 = crypto.createHash('md5'),
+        email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+        head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
+    var user = {
+        username:this.username,
+        password:this.password,
+        email:this.email,
+        tests:this.tests,
+        head:this.head
+    };
+    var newUser = new userModel(user);
+
+    newUser.save(function (err,user){
+        if(err) {
+            return callback(err);
+        }
+        callback(null,user);
+    });
+}
+
+User.get = function(name,callback){
+    userModel.findOne({name: name},function(err,user){
+        if(err) {
+            return callback(err);
+        }
+        callback(null,user);
+    });
+};
+
+module.exports = User;
